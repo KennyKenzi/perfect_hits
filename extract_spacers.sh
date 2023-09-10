cec1c3fb7ef47df47e73dfcaf9ed524a28021d0f
Kenny <kenny_ayangbile@gmail.com>
2023-09-09 22:14:59 -0400

    added example files

--- extract_spacers.sh
+++ extract_spacers.sh
@@ -2,7 +2,7 @@
 
 
 
-blastn -query "$1" -subject "$2" -task "blastn-short" -outfmt "6 sseqid pident length qlen" -out temp_file
+blastn -query "$1" -subject "$2" -task "blastn-short" -outfmt "6 std" -out temp_file
 
 awk '$2 == 100 && $3==$4' temp_file > "$3"
 wc -l "$3" 
